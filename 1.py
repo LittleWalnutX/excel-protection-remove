a147647526a8bfbcd94fd18b393d6668901986f7
xht8350 <xht8350@yeah.net>
2023-07-19 21:57:55 +0800

    修改

--- 1.py
+++ 1.py
@@ -19,17 +19,13 @@ wp_stop = xml.find(r"/>", wp_start + 1) + 2
 
 out_xml = xml[:wp_start] + xml[wp_stop:]
 
-#print(out_xml)
-
 os.remove(extract_dir + "/xl/workbook.xml")
-
-
 
 with open(extract_dir + "/xl/workbook.xml", "w") as f:
     f.write(out_xml)
 
 shutil.make_archive(in_file_name + "r", "zip", extract_dir)
-shutil.move(in_file_name + "r.zip", "rmed_pct.xlsx")
+shutil.move(in_file_name + "r.zip",  in_file_name + "rmed_pct.xlsx")
 
 shutil.rmtree(path=extract_dir)
 
--- README.md
+++ README.md
@@ -5,12 +5,12 @@ remove ani excel protection without a passwd
 ## usage
 
 ```bash
-python3 1.py input_filename
+python3 1.py $input_filename
 ```
 
 Don't need to enter a password
 
-And you will get a file `rmed_pct.xlsx` in `$PWD`, this is the file whose protection is removed.
+And you will get a file `${input_filename}rmed_pct.xlsx`, and this is the file whose protection is removed.
 
 
 
